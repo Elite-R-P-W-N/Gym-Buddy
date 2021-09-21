d0d78cd57cde815160a71be7692c356a6437836c
menonabhineet <menonabhineet@gmail.com>
2021-09-22 03:04:51 +0530

    -

--- app/src/main/java/com/example/gymbuddy/chest_workout.java
+++ app/src/main/java/com/example/gymbuddy/chest_workout.java
@@ -10,21 +10,21 @@ import android.view.animation.AnimationUtils;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
-public class WorkoutActivity extends AppCompatActivity {
+public class chest_workout extends AppCompatActivity {
 
     TextView titlepage, subtitlepage, intropage, subintropage, btnexercise, fitonetitle, fitonedesc, fittwotitle, fittwodesc, fitthreetitle,
-            fitthreedesc, fitfourtitle, fitfourdesc;
+            fitthreedesc, fitfourtitle, fitfourdesc, fitfivetitle, fitfivedesc, fitsixtitle, fitsixdesc;
 
     View divpage, bgprogress;
 
     Animation btone, bttwo, btfour, btfive, btsix, btseven, bteight;
 
-    LinearLayout fitone, fittwo, fitthree, fitfour;
+    LinearLayout fitone, fittwo, fitthree, fitfour, fitfive, fitsix;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_workout);
+        setContentView(R.layout.chest_workout);
 
         //Load Animation
         btone = AnimationUtils.loadAnimation(this,R.anim.btone);
@@ -48,11 +48,17 @@ public class WorkoutActivity extends AppCompatActivity {
         fitthreedesc = (TextView) findViewById(R.id.fitthreedesc);
         fitfourtitle = (TextView) findViewById(R.id.fitfourtitle);
         fitfourdesc = (TextView) findViewById(R.id.fitfourdesc);
+        fitfivetitle = (TextView) findViewById(R.id.fitfivetitle);
+        fitfivedesc = (TextView) findViewById(R.id.fitfivedesc);
+        fitsixtitle = (TextView) findViewById(R.id.fitsixtitle);
+        fitsixdesc = (TextView) findViewById(R.id.fitsixdesc);
 
         fitone = (LinearLayout) findViewById(R.id.fitone);
         fittwo = (LinearLayout) findViewById(R.id.fittwo);
         fitthree = (LinearLayout) findViewById(R.id.fitthree);
         fitfour = (LinearLayout) findViewById(R.id.fitfour);
+        fitfive = (LinearLayout) findViewById(R.id.fitfive);
+        fitsix = (LinearLayout) findViewById(R.id.fitsix);
 
         divpage = (View) findViewById(R.id.divpage);
         bgprogress = (View) findViewById(R.id.bgprogress);
@@ -69,17 +75,21 @@ public class WorkoutActivity extends AppCompatActivity {
         fittwo.startAnimation(btfour);
         fitthree.startAnimation(btfive);
         fitfour.startAnimation(btsix);
+        fitfive.startAnimation(btfive);
+        fitsix.startAnimation(btsix);
 
         btnexercise.startAnimation(bteight);
         bgprogress.startAnimation(btseven);
 
-        fitone.setOnClickListener(new View.OnClickListener(){
+        btnexercise.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
-                Intent a = new Intent(WorkoutActivity.this,chest_workout.class);
+                Intent a = new Intent(chest_workout.this,MainActivity.class);
                 a.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
                 startActivity(a);
             }
         });
     }
+
+
 }
